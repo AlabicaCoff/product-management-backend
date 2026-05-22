a57579cc20d60f8d2f738fa2ca787eeaff5a0b4d
Thanathat Pinthu <thanathatp2003@gmail.com>
2026-05-23 02:45:06 +0700

    feat: CategoryProfile for AutoMapper

--- ProductManagement.Api/Utilities/Interfaces/ISeeder.cs
+++
@@ -1,9 +0,0 @@
-using ProductManagement.Api.Data;
-
-namespace ProductManagement.Api.Utilities.Interfaces
-{
-    public interface ISeeder
-    {
-        Task SeedAsync(ApplicationDbContext context, IServiceProvider services);
-    }
-}

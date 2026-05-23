7e0668e115936c4efa4a9d130834f7872d0d1306
Thanathat Pinthu <thanathatp2003@gmail.com>
2026-05-24 01:42:21 +0700

    fix: add constraints to product entity and add migrations

--- ProductManagement.Api/Data/Migrations/20260523183237_Add_Constraints_to_Product.Designer.cs
+++ ProductManagement.Api/Data/Migrations/20260523183237_Add_Constraints_to_Product.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManagement.Api.Data;
 
@@ -11,9 +12,11 @@ using ProductManagement.Api.Data;
 namespace ProductManagement.Api.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260523183237_Add_Constraints_to_Product")]
+    partial class Add_Constraints_to_Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ProductManagement.Api/Models/Product.cs
+++ ProductManagement.Api/Models/Product.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductManagement.Api.Models
 {
@@ -6,9 +7,16 @@ namespace ProductManagement.Api.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Name cannot be empty.")]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock cannot be negative.")]
         public int Stock { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
